17adf810fab911f6c9809a69d822c69176d41238
Pallavi-s-r <pallavisingh.sgrl9@gmail.com>
2023-09-07 15:01:26 +0530

    inital setup for server clients as well as created model

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+const route = require('./routes/route');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log(`Express app running ${process.env.DEV_MODE} Modeon port`  + (process.env.PORT || 3000))
+});
